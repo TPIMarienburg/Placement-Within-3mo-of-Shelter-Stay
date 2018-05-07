4f42e04dc80ac6c6df4f0167834743bb5f9f4c86
TPI Marienburg <david.katz@tprojects.org>
2018-05-07 09:08:05 -0700

    Beta Commit v.1b
    All modules have been flushed out but have yet to be tested.  Expect a flurry of commits to follow.

--- placementspostshelter.py
+++ placementspostshelter.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import numpy as np
 
+from dateutil.relativedelta import relativedelta
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 
 class IdentifyPlacementsPostShelterStay:
     """
     This class will process the Placement Report v.4 + Entry to Shelter.xlsx report that is an ART
-    product.
+    product. As of the writing of this script the ART report is located in the following location:
+    Public Folder/City of Portland/TPI/W.I.P./
 
     The goal is to create a list of addresses for participants who were placed into housing within
     3 months of a shelter stay.
     """
     def __init__(self, file):
+        """
+        Initializes the class but also processes the raw excel file into three distinct data frames
+        removing rows from each of the data frames where the relevant date fields are blank.
+
+        :file: the location of the Placement Report v.4 + Entry to Shelter.xlsx
+        """
         self.file = file
-        self.raw_entries = pd.read_excel(self.file, sheet_name="Entries to Shelter")
-        self.raw_placements = pd.read_excel(self.file, sheet_name="Placement Data")
-        self.raw_addresses = pd.read_excel(self.file, sheet_name="Address Data")
+        self.raw_entries = pd.read_excel(self.file, sheet_name="Entries to Shelter").dropna(
+            axis=0,
+            how="any",
+            subset="Entry Exit Exit Date"
+        )
+        self.raw_placements = pd.read_excel(self.file, sheet_name="Placement Data").dropna(
+            axis=0,
+            how="any",
+            subset="Placement Date(3072)"
+        )
+        self.raw_addresses = pd.read_excel(self.file, sheet_name="Address Data").dropna(
+            axis=0,
+            how="any",
+            subset="Placement Date(833)"
+        )
 
     def find_related_data(
         self,
@@ -34,7 +54,11 @@ class IdentifyPlacementsPostShelterStay:
         :placements: The raw_placements dataframe
         :addresses:  The raw_adresses dataframe
         """
-        pass
+        # add a line dropping entry rows if they don't have an exit date
+        valid_entries = entries[entries["Client Uid"].isin(placements["Client Uid"])]
+        valid_placements = placements[placements["Client Uid"].isin(valid_entries["Client Uid"])]
+        valid_addresses = addresses[addresses["Client Uid"].isin(valid_placements)]
+        return valid_entries, valid_placements, valid_addresses
 
     def check_for_entry_3_months_prior_to_placement(self, entry_data, placement_data):
         """
@@ -47,7 +71,22 @@ class IdentifyPlacementsPostShelterStay:
         :placement_data: A placement dataframe processed to only contain rows with a Client Uid in the
         entries dataframe
         """
-        pass
+        merged = pd.merge(placement_data, entry_data, how="outer", on="Client Uid")
+        merged["Entry Exit Entry Date"] = merged["Entry Exit Entry Date"].dt.date
+        merged["Entry Exit Exit Date"] = merged["Entry Exit Exit Date"].dt.date
+        merged["Placement Date(3072)"] = merged["Placement Date(3072)"].dt.date
+        merged["3mo Pre Placement"] = merged["Placement Date(3072)"] + relativedelta(month=-3)
+        in_range = merged[
+            (
+                (merged["Entry Exit Exit Date"] < merged["Placement Date(3072)"]) |
+                (merged["Entry Exit Exit Date"] == merged["Placement Date(3072)"])
+            ) &
+            (
+                (merged["Entry Exit Exit Date"] > merged["3mo Pre Placement"]) |
+                (merged["Entry Exit Exit Date"] == merged["3mo Pre Placement"])
+            )
+        ]
+        return in_range
 
     def find_closest_address(self, placement_data):
         """
@@ -59,7 +98,17 @@ class IdentifyPlacementsPostShelterStay:
         check_for_entry_3_months_prior_to_placement methodself.
         :address_data: The address data_frame that is produced by the find_related_data method.
         """
-        pass
+        merged = pd.merge(placement_data, self.raw_addresses, how="outer", on="Client Uid")
+        merged["Placement Date(833)"] = merged["Placement Date(833)"].dt.date
+        in_range = merged[
+            (merged["Placement Date(833)"] == merged["Placement Date(3072)"]) |
+            (merged["Placement Date(833)"] < merged["Placement Date(3072)"])
+        ].sort_values(by="Placement Date(833)", ascending=False).drop_duplicates(subset="Client Uid")
+        return self.raw_addresses[
+            self.raw_addresses["Recordset ID (61-recordset_id)"].isin(
+                in_range["Recordset ID (61-recordset_id)"]
+            )
+        ]
 
     def process(self):
         """
@@ -75,6 +124,7 @@ class IdentifyPlacementsPostShelterStay:
         return find_closest_address(post_shelter_placement)
 
 if __name__ == "__main__":
+    # report location: \\tproserver\Reports\OneOff\DavidKatz\FY17-18\Placements Post Shelter Stay
     report = askopenfilename(
         title="Open the Placement Report v.4 + Entry to Shelter.xlsx ART report"
     )
